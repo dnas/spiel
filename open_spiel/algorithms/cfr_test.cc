409c66f8cc0ae7695adc7f32256678457fc124df
dnas <dolas.nassauer@gmail.com>
2024-05-03 04:15:37 +0200

    Allow for any number of cards, fix nash value

--- open_spiel/algorithms/cfr_test.cc
+++ open_spiel/algorithms/cfr_test.cc
@@ -69,7 +69,7 @@ void CheckNashSimplePoker(const Game& game, const Policy& policy) {
       ExpectedReturns(*game.NewInitialState(), policy, -1);
 
   // 1/18 is the Nash value. See https://en.wikipedia.org/wiki/Kuhn_poker
-  constexpr float nash_value = 0.06;
+  constexpr float nash_value = 0.061;
   constexpr float eps = 1e-3;
 
   std::cout << game_value[0] << ", " << game_value[1] << std::endl;
